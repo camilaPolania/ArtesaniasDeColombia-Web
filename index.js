a9f099809053759f58630a441690df0f23e6e245
unknown <camila@pc.com>
2018-11-29 09:22:51 -0500

    Publicado

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const dbName = 'productos';
 const client = new MongoClient(url);
 var db = null;
 
-MongoClient.client('mongodb+srv://cluster0-ecypx.mongodb.net/productos',{
-    auth:{
-        user:'Polania_98',
+MongoClient.connect('mongodb+srv://cluster0-ecypx.mongodb.net/productos', {
+    auth: {
+        user: 'Polania_98',
         password: 'daniVegano'
     }
-},function(err, client){
-    if(err)throw err;
-    db= client.db('productos');
+}, function (err, client) {
+    if (err) throw err;
+    db = client.db('productos');
     app.listen(process.env.PORT || 1234);
 });
 
