f0997d669aac807305b29a9eeec843e28df5ebc0
unknown <camila@pc.com>
2018-11-28 19:32:32 -0500

    animacion portada

--- public/style/animation.js
+++ public/style/animation.js
@@ -5,15 +5,24 @@ var sizes = ["small", "medium", "large" ],
 	endY = -100;
 
 var particules = new TimelineMax();
-
+console.log("holaaaa");
 for(var i=0; i<50; i++){
-	var sizeIndex = randomNumber(0,2);
+	console.log("holaaaa");
+	var sizeIndex = Math.random()*2;
 	var size = sizes[sizeIndex];
-	var speed = (3 - sizeIndex);
-	var bubble = $('<div calss="bubble ' + size + 'Bubble"/>').appendTo(bubbleContainer);
+	var speed = (3 - sizeIndex)
+	var bubble= $('<div class="bubble ' + size + 'Bubble"/>').appendTo(bubbleContainer);
 	particules.set(bubble, {y:startY, x:100}, 0);
 
-	particules.to(bubble, speed, {y:endY, x:randomNumber(0,300),
+	particules.to(bubble, speed, {y:endY, x:Math.random()*300,
 	repeatDelay:Math.random()*2, repear:500}, Math.random() *2)
 }
 
+normal.onclick = function() {
+	TweenLite.to(particles, 2, {timeScale:1, ease:Linear.easeNone}); // normal speed
+  }
+
+  function randomNumber(min, max){
+	return Math.floor(Math.random() * (1 + max - min) + min);
+}
+	
